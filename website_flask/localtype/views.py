e0528242263c308e12b55feadff1f6d815eab097
Nicholas Knezek <nknezek@gmail.com>
2018-06-14 17:55:04 -0700

    working synonyms

--- website_flask/localtype/views.py
+++ website_flask/localtype/views.py
@@ -11,6 +11,8 @@ from sklearn.pipeline import make_pipeline
 from lime.lime_text import LimeTextExplainer
 import dill
 
+import localtype.synonyms as syn
+
 tokenizer = RegexpTokenizer(r'[a-zA-Z]+')
 stemmer = SnowballStemmer('english')
 tfidf_vectorizer = dill.load(open('/Users/nknezek/Documents/Insight_local/project/3city_test/tfidf_vectorizer.m', 'rb'))
@@ -20,6 +22,7 @@ c = dill.load(open("/Users/nknezek/Documents/Insight_local/project/3city_test/tr
 
 statetowns = ['AK/Anchorage/', 'CA/Berkeley/', 'TX/Denton/']
 explainer = LimeTextExplainer(class_names=statetowns)
+
 
 @app.route('/')
 @app.route('/index')
@@ -38,11 +41,13 @@ def text_output():
     # pull input text and city from input field and store it
     input_text = request.args.get('input_text')
     input_city = request.args.get('input_city')
+    syndict = syn.suggest_synonyms(c,tokenizer,input_text,int(input_city))
+    synhtml = syn.html_suggested_synonyms(syndict)
     if input_text is None:
         input_text = "you didn't enter any text! So instead you get to see this easter-egg! Aren't you lucky?"
     exp = explainer.explain_instance(input_text, c.predict_proba, num_features=6, labels=[int(input_city)])
 
     # just select the Cesareans  from the birth dtabase for the month that the user inputs
 
-    return render_template("output.html", input_text=input_text, input_city=input_city, explainer=exp.as_html())
+    return render_template("output.html", input_text=input_text, input_city=input_city, explainer=exp.as_html(), synonyms=synhtml)
 
